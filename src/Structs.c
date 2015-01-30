a3436dbe97515fbc14c7ed23743439087e3b511c
Dennis Kupec <denniskupec@gmail.com>
2015-01-30 17:56:29 -0600

    Create Structs.c

---
+++ src/Structs.c
@@ -0,0 +1,16 @@
+
+struct RPMValues
+{
+	int playerbase;
+	int lifestate;
+	int onground;
+	int teamid;
+};
+
+struct Process
+{
+	HANDLE process;
+	HWND window;
+	int pid;
+	int modbase_client;
+};
