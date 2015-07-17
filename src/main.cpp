fe0222ce9967bd2d077330f9859ef318a15e9864
Dennis Kupec <denniskupec@gmail.com>
2015-07-16 21:57:29 -0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include "main.h"
+#include "Local.h"
+
+using std::cout;
+
+Local CSGO = Local("Counter-Strike: Global Offensive");
+
+int main(void)
+{
+	cout << "Process ID: " << CSGO.pid << "\n";
+
+	while (1) {
+		CSGO.LocalPlayer = (DWORD)CSGO.Read<DWORD>(CSGO.ModuleBase[0] + Local::m_dwLocalPlayer);
+		
+		if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && CSGO.onGround()) {
+				SendMessage(CSGO.window, WM_KEYDOWN, VK_SPACE, 0x390000);
+				SendMessage(CSGO.window, WM_KEYUP, VK_SPACE, 0x390000);
+		}
+
+		Sleep(1);
+	}
+
+	return 0;
+}
+
