ed246e31488d480ec20ca510d21af5ba806d2f4e
Dennis Kupec <denniskupec@gmail.com>
2015-07-18 20:08:14 -0500

    Update Local.h

--- src/Local.h
+++ src/Local.h
@@ -10,10 +10,6 @@ public:
 	Local(const char* window_title);
 	virtual ~Local();
 
-	static const DWORD m_dwLocalPlayer = 0x00A77D2C;
-	static const DWORD m_fFlags = 0x00000100;
-	static const DWORD m_dwMaxPlayer = 0x000002F0;
-
 	DWORD ModuleBase[2];
 	DWORD pid;
 	HANDLE handle;
