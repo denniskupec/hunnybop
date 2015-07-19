ee5cde7606aeb5f6c452399c73bf0215f71ed145
Dennis Kupec <denniskupec@gmail.com>
2015-07-18 20:10:38 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,16 @@ int main(void)
 	cout << "Process ID: " << CSGO.pid << "\n";
 
 	while (1) {
-		CSGO.LocalPlayer = (DWORD)CSGO.Read<DWORD>(CSGO.ModuleBase[0] + Local::m_dwLocalPlayer);
+		CSGO.LocalPlayer = (DWORD)CSGO.Read<DWORD>(CSGO.ModuleBase[0] + 0x00A77D2C);
 		
 		if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && CSGO.onGround()) {
-				SendMessage(CSGO.window, WM_KEYDOWN, VK_SPACE, 0x390000);
-				SendMessage(CSGO.window, WM_KEYUP, VK_SPACE, 0x390000);
+			SendMessage(CSGO.window, WM_KEYDOWN, VK_SPACE, 0x390000);
+			SendMessage(CSGO.window, WM_KEYUP, VK_SPACE, 0x390000);
+			Sleep(17);
+			SendMessage(CSGO.window, WM_KEYDOWN, VK_SPACE, 0x390000);
+			SendMessage(CSGO.window, WM_KEYUP, VK_SPACE, 0x390000);
 		}
-
+		
 		Sleep(1);
 	}
 
